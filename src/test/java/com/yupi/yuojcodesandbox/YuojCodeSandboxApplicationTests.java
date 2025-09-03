6b17e91d59b9f49e92b7f25594c28084fe50921c
huixin <2926800497@qq.com>
2025-09-03 10:39:29 +0800

    第五期完成实现java原生代码沙箱

---
+++ src/test/java/com/yupi/yuojcodesandbox/YuojCodeSandboxApplicationTests.java
@@ -0,0 +1,15 @@
+package com.yupi.yuojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest
+class YuojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
